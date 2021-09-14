7b4f0da964ad7e0e003a5a660735156aa6725a38
Wahed Nur <wahednur@gmail.com>
2021-09-14 08:38:24 +0600

    Redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,40 @@
-/* Desktop search expension function start */
-const searchIcon = document.querySelector('.search-group__icon');
+/* Desktop search bar on clik active javascript start */
+const searchBtn = document.querySelector('.search-btn');
 const searchHover = document.querySelector('.input-control');
 
-searchIcon.addEventListener('mouseover', () => {
+searchBtn.addEventListener('mouseover', () => {
     searchHover.style.width = '100%';
-    
 });
-searchIcon.addEventListener('mouseout', () => {
+
+searchBtn.addEventListener('mouseout', () => {
     searchHover.style.width = '';
-    
 });
-/* Desktop search expension function end */
-
-/* Desktop menu ative function start */
-const menuBtn = document.querySelectorAll('.menu-search .desk-nav__link');
-const active = document.querySelectorAll('.desk-nav__menu-item');
-menuBtn.forEach((item, indx) => {
-    item.addEventListener('click', () => {
-        menuAction()
-        active[indx].classList.add('active')
-    })
-})
-function menuAction(){
-    active.forEach(item => item.classList.remove('active'));
-    
-};
-
-/* Desktop menu ative function esnd */
-
-/* Mobile active menu function start  */
-
-
-const mobileMenuBtn = document.querySelectorAll('.menu-search .mobile-nav__link');
-const mobileMenuItem = document.querySelectorAll('.menu-search .mobile-nav__item');
-
-mobileMenuBtn.forEach((item, indx) => {
-    item.addEventListener('click', () => {
-        removeActive()
-        mobileMenuItem[indx].classList.add('active')
-        mobileSearch.classList.remove('active')
-        dMenu.classList.remove('active')
-        
+/* Desktop search bar on clik active javascript end */
+/* Desktop Menu on clik active javascript start */
+const deskMnuBtn = document.querySelectorAll('.desktop-nav .nav-search .desk-nav__link')
+const deskNavActive = document.querySelectorAll('.desk-nav__link')
+deskMnuBtn.forEach((item, indx) => {
+    item.addEventListener('click', () =>{
+        removeActive();
+        deskNavActive[indx].classList.add('active');
     })
 })
 
 function removeActive(){
-    mobileMenuItem.forEach(item => item.classList.remove('active'));
-    
-    
+    deskNavActive.forEach(item => item.classList.remove('active'));
 }
-const showMenuBtn = document.querySelector('.showBtnmenu');
-const dMenu = document.querySelector('.mobile-menu');
-showMenuBtn.addEventListener('click', () => {
-    dMenu.classList.toggle('active');
-    
-   
-})
+/* Desktop Menu on clik active javascript end */
 
-const mobileSearch = document.querySelector('.mobile-search');
-const mobileSearchBtn = document.querySelector('.msearchBtn');
-mobileSearchBtn.addEventListener('click', () => {
-    mobileSearch.classList.toggle('active');
-    
-})
-
-
-/* Mobile active menu function end  */
-
-
-
-
-
+/* Mobile menu on clik active javascript start */
+const mblNavBtn = document.querySelectorAll('.mobile-nav nav .mbl-nav__mbl-link')
+const mblNavActive = document.querySelectorAll('.mbl-nav__mbl-link')
+mblNavBtn.forEach((item, indx) =>{
+    item.addEventListener('click', () => {
+        removeBmlNavActive();
+        mblNavActive[indx].classList.add('active');
+    });
+});
+function removeBmlNavActive(){
+    mblNavActive.forEach(item => item.classList.remove('active'))
+}
+/* Mobile menu on clik active javascript end */
