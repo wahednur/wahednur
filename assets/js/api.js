376e0c3c33465b7b78b80c78c982171ddf7c0652
Wahed Nur <wahednur@gmail.com>
2021-09-23 04:54:39 +0600

    Blog section added

--- assets/js/api.js
+++ assets/js/api.js
@@ -41,3 +41,47 @@ $('.feedback').slick({
     // instead of a settings object
   ]
 });
+
+$('.blog').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fal fa-chevron-right' aria-hidden='true'></i></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right'><i class='fal fa-chevron-left' aria-hidden='true'></i></button>",
+  
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      arrows:false,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      arrows:false,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
